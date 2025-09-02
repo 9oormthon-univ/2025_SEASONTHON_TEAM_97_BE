8abc062805d63991f4b869efa74d7901625d9972
YejiJeonn <yejijeon02@gmail.com>
2025-09-02 23:50:41 +0900

    :tada: init: first commit

---
+++ src/main/java/com/example/ccsketch/YouthSketchBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ccsketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthSketchBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouthSketchBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ccsketch/YouthSketchBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ccsketch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthSketchBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
