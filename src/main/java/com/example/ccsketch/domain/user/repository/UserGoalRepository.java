38101aa2773abd8132ca9f266bae744d956c927c
lmj <skadia23@inu.ac.kr>
2025-09-07 07:14:31 +0900

    jpa bug fix plz

--- src/main/java/com/example/ccsketch/domain/user/repository/UserGoalRepository.java
+++ src/main/java/com/example/ccsketch/domain/user/repository/UserGoalRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserGoalRepository extends JpaRepository<UserGoal, Long> {
-    List<UserGoal> findByUserId(Long userId);
+    List<UserGoal> findByUser_UserId(Long userId);
 }
